1bfedb6543843bf99cd7483fa861c9b9bac3ea6d
peterosoo <posoo617@gmail.com>
2021-03-26 12:52:51 +0300

    router links

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+      placeat saepe, numquam sunt velit odit dignissimos ex perferendis?
+      Doloremque facere est enim nam nemo laboriosam quisquam incidunt a ipsa
+      pariatur quibusdam, porro autem nisi quasi veniam vero! Veritatis soluta
+      consequatur explicabo commodi possimus aliquid deserunt delectus non ex
+      sapiente maiores nulla, deleniti aut? Provident nobis dolor numquam
+      facere! Saepe eaque dignissimos esse pariatur ducimus neque nam dolorum
+      quis, possimus beatae?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+      voluptate dicta recusandae tempore, totam soluta fugit quidem? Doloribus
+      dignissimos ipsam, ex nihil ab sed similique porro! Repellendus minus
+      placeat eveniet unde! Magni dicta voluptates veniam quidem earum quas
+      fuga, doloremque optio cum inventore exercitationem? Quae enim quibusdam,
+      incidunt odit voluptatum eaque dicta numquam, expedita corporis quos
+      distinctio ex quaerat ipsa, alias facilis provident ullam. Neque,
+      necessitatibus placeat.
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Home Page</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+      placeat saepe, numquam sunt velit odit dignissimos ex perferendis?
+      Doloremque facere est enim nam nemo laboriosam quisquam incidunt a ipsa
+      pariatur quibusdam, porro autem nisi quasi veniam vero! Veritatis soluta
+      consequatur explicabo commodi possimus aliquid deserunt delectus non ex
+      sapiente maiores nulla, deleniti aut? Provident nobis dolor numquam
+      facere! Saepe eaque dignissimos esse pariatur ducimus neque nam dolorum
+      quis, possimus beatae?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+      voluptate dicta recusandae tempore, totam soluta fugit quidem? Doloribus
+      dignissimos ipsam, ex nihil ab sed similique porro! Repellendus minus
+      placeat eveniet unde! Magni dicta voluptates veniam quidem earum quas
+      fuga, doloremque optio cum inventore exercitationem? Quae enim quibusdam,
+      incidunt odit voluptatum eaque dicta numquam, expedita corporis quos
+      distinctio ex quaerat ipsa, alias facilis provident ullam. Neque,
+      necessitatibus placeat.
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
